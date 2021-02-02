e8bad18590efdbf16b06c95931bfc00ee13b0558
Caglan Atay <caglanatay@windowslive.com>
2021-02-03 00:01:31 +0300

    Methodlar düzenlendi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,56 +14,27 @@ namespace ConsoleUI
             ICarService carManager = new CarManager(new InMemoryCarDal());
 
             Console.WriteLine("-----------------------Tüm arabalar listeleniyor----------------------------------");
-            // GetAll() ile Tüm arabalar listeleniyor
-            
-            foreach (var car in carManager.GetAll())
-            {
-               Console.WriteLine("{0}. Araba Model = {1}, Renk = {2} ,Yıl = {3} ,Fiyat = {4} ,Açıklama = {5}",
-                    car.Id, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description);               
-                
-            }
 
+            GetAllList(carManager);
 
             Console.WriteLine("------------------Id si belirtilen araba yazdırılıyor-------------------------------");
 
-
-            // GetById ile belirtilen id de araba listeden alınıyor.
             Car car1 = carManager.GetById(2);
-            
-            Console.WriteLine("{0}. Araba Model = {1}, Renk = {2} ,Yıl = {3} ,Fiyat = {4} ,Açıklama = {5}",
-                    car1.Id, car1.BrandId, car1.ColorId, car1.ModelYear, car1.DailyPrice, car1.Description);
+            PrintSingleElement(car1);
 
+            Console.WriteLine("-----------------Araba güncelleştiriliyor ve yazdırılıyor---------------------------");
 
-            Console.WriteLine("------------------araba güncelleştiriliyor ve yazdırılıyor---------------------------");
-
-
-            // Arabanın yeni değer giriliyor ve güncelleştiriliyor.
-            
-            car1.DailyPrice = 240.900;            
+            car1.DailyPrice = 240.900;
             carManager.Update(car1);
-           
-            Console.WriteLine("{0}. Araba Model = {1}, Renk = {2} ,Yıl = {3} ,Fiyat = {4} ,Açıklama = {5}",
-                    car1.Id, car1.BrandId, car1.ColorId, car1.ModelYear, car1.DailyPrice, car1.Description);
+            PrintSingleElement(car1);
 
+            Console.WriteLine("-----------------Araba listeden silinip araba listesi tekrar yazdırılıyor---------------");
 
-            Console.WriteLine("----------------------Araba listeden silinip araba listesi tekrar yazdırılıyor------------------------------------");
-
-
-            // Belirtilen araba List içinden siliniyor
             carManager.Delete(car1);
-            
-            foreach (var car in carManager.GetAll())
-            {
-                Console.WriteLine("{0}. Araba Model = {1}, Renk = {2} ,Yıl = {3} ,Fiyat = {4} ,Açıklama = {5}",
-                     car.Id, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description);
+            GetAllList(carManager);
 
-            }
+            Console.WriteLine("-----------------Yeni araba eklendikten sonra liste yazdırılıyor----------------------");
 
-
-            Console.WriteLine("-------------------Yeni araba eklendikten sonra liste yazdırılıyor---------------------------------");
-
-
-            // Yeni araba oluşturuluyor ve List içine ekleniyor ve arabalar tekrardan ekrana yazdırılıyor
             Car newCar = new Car();
             newCar.Id = 7;
             newCar.BrandId = 7;
@@ -73,16 +44,23 @@ namespace ConsoleUI
             newCar.Description = "Peugeot Allure";
             carManager.Add(newCar);
 
-            
+            GetAllList(carManager);
+        }
+
+        private static void PrintSingleElement(Car car)
+        {
+            Console.WriteLine("{0}. Araba Model = {1}, Renk = {2} ,Yıl = {3} ,Fiyat = {4} ,Açıklama = {5}",
+                                car.Id, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description);
+        }
+
+        private static void GetAllList(ICarService carManager)
+        {
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine("{0}. Araba Model = {1}, Renk = {2} ,Yıl = {3} ,Fiyat = {4} ,Açıklama = {5}",
                      car.Id, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description);
 
             }
-            Console.WriteLine("----------------------------------------------------------------------------------");
-
-
         }
     }
 }
